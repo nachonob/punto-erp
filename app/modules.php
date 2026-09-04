5dcdc17bff438a2ba253a7df5c82bf84d1e880dd
nachonob <escobarignacio@gmail.com>
2026-09-04 15:56:21 -0300

    Habilitar módulo Presupuestos en DEV

--- app/modules.php
+++ app/modules.php
@@ -3,7 +3,7 @@ return [
  'accounts'=>['name'=>'Cuentas por cobrar','enabled'=>true,'entry'=>'app/Modules/AccountsReceivable/module.php'],
  'crm'=>['name'=>'Clientes y CRM','enabled'=>false],
  'projects'=>['name'=>'Gestión de proyectos','enabled'=>false],
- 'quotes'=>['name'=>'Presupuestos','enabled'=>false],
+ 'quotes'=>['name'=>'Presupuestos','enabled'=>true,'entry'=>'app/Modules/Quotes/module.php'],
  'arca'=>['name'=>'Facturación ARCA','enabled'=>false],
  'purchases'=>['name'=>'Compras y proveedores','enabled'=>false],
  'inventory'=>['name'=>'Stock y materiales','enabled'=>false],
