ba323299157f33a8f0a9f8df0b4e1705ae6d37ad
nachonob <escobarignacio@gmail.com>
2026-09-04 15:54:00 -0300

    Enrutar alta de presupuestos al módulo multimarca en DEV

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 declare(strict_types=1);
+$a=$_GET['a']??'';
+if(in_array($a,['new_quote','save_quote'],true)){
+ require __DIR__.'/app/Modules/Quotes/module.php';
+ exit;
+}
 $module=$_GET['module']??'accounts';
 $registry=require __DIR__.'/app/modules.php';
 if(!isset($registry[$module])){http_response_code(404);exit('Módulo inexistente.');}
