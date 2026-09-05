b6b7946fc2522852b8026281ae99c927540e3ad5
nachonob <escobarignacio@gmail.com>
2026-09-05 09:05:16 -0300

    Usar nuevo editor de presupuestos con busqueda SKU descripcion

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ if($a==='quotes'){
  require __DIR__.'/app/Modules/Quotes/list_v2.php';
  exit;
 }
-if(in_array($a,['new_quote','save_quote','quote_view','quote_print'],true)){
- require __DIR__.'/app/Modules/Quotes/module_v3.php';
+if(in_array($a,['new_quote','save_quote','edit_quote','update_quote','quote_view','quote_print'],true)){
+ require __DIR__.'/app/Modules/Quotes/module_v4.php';
  exit;
 }
 if(in_array($a,['products','new_product','edit_product','save_product','update_product','price_lists','save_price_list','update_price_list','product_categories','save_product_category','update_product_category','stock_movement','save_stock_movement'],true)){
