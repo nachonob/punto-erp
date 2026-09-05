92b07eca9443c776c56f957554394850565370f8
nachonob <escobarignacio@gmail.com>
2026-09-05 10:35:06 -0300

    Mostrar diseño final del presupuesto al guardar

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ if($a==='quotes'){
  require __DIR__.'/app/Modules/Quotes/list_v2.php';
  exit;
 }
-if(in_array($a,['new_quote','save_quote','edit_quote','update_quote','quote_view','quote_print'],true)){
+if(in_array($a,['quote_view','quote_print'],true)){
+ require __DIR__.'/app/Modules/Quotes/print_v2.php';
+ exit;
+}
+if(in_array($a,['new_quote','save_quote','edit_quote','update_quote'],true)){
  require __DIR__.'/app/Modules/Quotes/module_v5.php';
  exit;
 }
