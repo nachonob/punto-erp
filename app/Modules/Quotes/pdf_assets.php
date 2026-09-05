719d1cf3d3c670806395eef7aeab8c9932b9d28c
nachonob <escobarignacio@gmail.com>
2026-09-05 11:19:43 -0300

    Agregar configuracion de prologos para PDF

---
+++ app/Modules/Quotes/pdf_assets.php
@@ -0,0 +1,6 @@
+<?php
+return [
+ 'lifesmart'=>__DIR__.'/assets/prologos/lifesmart.pdf',
+ 'control4'=>__DIR__.'/assets/prologos/control4.pdf',
+ 'shelly'=>__DIR__.'/assets/prologos/shelly.pdf',
+];
