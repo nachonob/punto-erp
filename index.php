fed9defbb3949d09b0958c7146bd353749248cad
nachonob <escobarignacio@gmail.com>
2026-09-05 09:26:27 -0300

    Usar presupuesto con precio unitario editable

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if($a==='quotes'){
  exit;
 }
 if(in_array($a,['new_quote','save_quote','edit_quote','update_quote','quote_view','quote_print'],true)){
- require __DIR__.'/app/Modules/Quotes/module_v4.php';
+ require __DIR__.'/app/Modules/Quotes/module_v5.php';
  exit;
 }
 if(in_array($a,['products','new_product','edit_product','save_product','update_product','price_lists','save_price_list','update_price_list','product_categories','save_product_category','update_product_category','stock_movement','save_stock_movement'],true)){
