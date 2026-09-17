ac92244936cd197b3244b26ef6d1b327651c4df1
nachonob <escobarignacio@gmail.com>
2026-09-17 10:47:30 -0300

    Registrar envío desde botones de mail y WhatsApp

---
+++ app/Modules/Quotes/print_v12.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+$id=(int)($_GET['id']??0);
+ob_start();
+require __DIR__.'/print_v11.php';
+$html=ob_get_clean();
+
+$html=preg_replace_callback('/href="(mailto:[^"]+)"/',function(array $match)use($id):string{
+    $target=htmlspecialchars_decode($match[1],ENT_QUOTES);
+    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=email&amp;target='.rawurlencode(base64_encode($target)).'"';
+},$html)??$html;
+$html=preg_replace_callback('/href="(https:\/\/wa\.me\/[^"]+)"/',function(array $match)use($id):string{
+    $target=htmlspecialchars_decode($match[1],ENT_QUOTES);
+    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=whatsapp&amp;target='.rawurlencode(base64_encode($target)).'"';
+},$html)??$html;
+
+echo $html;
