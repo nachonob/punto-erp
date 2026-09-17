07595eeb38dd748c49109f746035aa18ce1bd3a2
nachonob <escobarignacio@gmail.com>
2026-09-17 10:47:40 -0300

    Proteger registro de envío con CSRF

--- app/Modules/Quotes/print_v12.php
+++ app/Modules/Quotes/print_v12.php
@@ -1,18 +1,18 @@
 <?php
 declare(strict_types=1);
 
-$id=(int)($_GET['id']??0);
+$id=(int)($_GET['id']??0);$csrf=(string)($_SESSION['csrf']??'');
 ob_start();
 require __DIR__.'/print_v11.php';
 $html=ob_get_clean();
 
-$html=preg_replace_callback('/href="(mailto:[^"]+)"/',function(array $match)use($id):string{
+$html=preg_replace_callback('/href="(mailto:[^"]+)"/',function(array $match)use($id,$csrf):string{
     $target=htmlspecialchars_decode($match[1],ENT_QUOTES);
-    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=email&amp;target='.rawurlencode(base64_encode($target)).'"';
+    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=email&amp;csrf='.rawurlencode($csrf).'&amp;target='.rawurlencode(base64_encode($target)).'"';
 },$html)??$html;
 $html=preg_replace_callback('/href="(https:\/\/wa\.me\/[^"]+)"/',function(array $match)use($id):string{
     $target=htmlspecialchars_decode($match[1],ENT_QUOTES);
-    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=whatsapp&amp;target='.rawurlencode(base64_encode($target)).'"';
+    return 'href="?a=send_quote&amp;id='.$id.'&amp;channel=whatsapp&amp;csrf='.rawurlencode($csrf).'&amp;target='.rawurlencode(base64_encode($target)).'"';
 },$html)??$html;
 
 echo $html;
