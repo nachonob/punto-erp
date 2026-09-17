571b6b1bb7a3e2ccce2ae2e65c916be401392e93
nachonob <escobarignacio@gmail.com>
2026-09-17 10:51:33 -0300

    Inicializar token para botones de envío

--- app/Modules/Quotes/print_v12.php
+++ app/Modules/Quotes/print_v12.php
@@ -5,7 +5,7 @@ $id=(int)($_GET['id']??0);
 ob_start();
 require __DIR__.'/print_v11.php';
 $html=ob_get_clean();
-$csrf=(string)($_SESSION['csrf']??'');
+$_SESSION['csrf']??=bin2hex(random_bytes(24));$csrf=(string)$_SESSION['csrf'];
 
 $html=preg_replace_callback('/href="(mailto:[^"]+)"/',function(array $match)use($id,$csrf):string{
     $target=htmlspecialchars_decode($match[1],ENT_QUOTES);
