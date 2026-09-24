19c3539e93993e88852f24b169a92df408810e97
nachonob <escobarignacio@gmail.com>
2026-09-24 14:20:37 -0300

    Normalizar catálogo al editar presupuestos

--- app/Modules/Quotes/module_v26.php
+++ app/Modules/Quotes/module_v26.php
@@ -2,6 +2,15 @@
 declare(strict_types=1);
 
 $a=$_GET['a']??'new_quote';
+$root=dirname(__DIR__,3);
+try{
+    require_once $root.'/app/Services/AdiProductImport.php';
+    $nameCfg=require $root.'/config.php';
+    $nameDb=new PDO('mysql:host='.$nameCfg['db_host'].';dbname='.$nameCfg['db_name'].';charset=utf8mb4',$nameCfg['db_user'],$nameCfg['db_pass'],[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC]);
+    normalizeProductCatalogFields($nameDb);
+    normalizeUnifiedProductNamesV2($nameDb);
+}catch(Throwable $e){}
+
 
 function sanitizeQuoteNotesHtml(string $html):string
 {
